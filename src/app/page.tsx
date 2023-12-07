2d4297ade87084bbc385698f3e3cef6ac92056e2
Ben Leffler <btleffler@gmail.com>
2023-12-06 19:26:59 -0500

    Fix link on main page and remove dummy files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="/about"
+          href="about"
+          target="_blank"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
